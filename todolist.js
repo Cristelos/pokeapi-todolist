e91c5675108545611912e914477add2e418c8cbf
Cristelos <cristelos@msn.com>
2023-03-14 14:09:43 +0100

    add some fixes to the js

--- todolist.js
+++ todolist.js
@@ -8,6 +8,32 @@ const lista$$ = document.querySelector('.li-container')
 
 //Una vez que los tengamos, tenemos que darle funcionalidad a los botones y crear un elemento li por cada texto introducido en el input
 
+//Creamos un array para añadir los elementos que vamos a ir creando
 
+const tareas = [];
 
-// Hay que añadir un botón de borrado para cada una de estas tareas
+// Vamos a crear una función que escuche el input, cree la lista y añada la p a una lista
+
+const crearLista = (devuelve) => {
+
+    //primero creamos una variable que nos dé el valor del input
+    const crearTarea = input$$.value;
+    if(crearTarea !== ""){
+        //creamos un elemento li
+        const nuevaLista$$ = document.createElement('li');
+        nuevaLista$$.innerText = crearTarea;
+
+        //creamos un botón para eliminar el punto de la lista
+        // const btnEliminar$$ = document.createElement('button');
+        // btnEliminar$$.innerText = 'X';
+        // btnEliminar$$.setAttribute('class','eliminar');
+        // nuevaLista$$.appendChild(btnEliminar$$);
+        lista$$.appendChild(nuevaLista$$);
+
+        input$$.value ='';
+
+    }
+}
+
+btn$$.addEventListener('click',crearLista)
+
